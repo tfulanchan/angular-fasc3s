cd78404041688d6958b89faaf46f5beec7ab5149
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-04-09 13:24:33 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AboutComponent } from './about/about.component';
+import { AcademicsComponent } from './academics/academics.component';
 
 @NgModule({
   imports: [
@@ -35,7 +37,9 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    AboutComponent,
+    AcademicsComponent,
   ],
   bootstrap: [ AppComponent ]
 })
