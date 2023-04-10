45ea43603768bdd22e7adba89023cb6ca60008a1
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-04-10 06:13:32 -0400

    Add files via upload

---
+++ src/app/attempts/attempts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attempts',
+  templateUrl: './attempts.component.html',
+  styleUrls: ['./attempts.component.css']
+})
+export class AttemptsComponent {
+
+}
