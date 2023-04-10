38b89b318ea7c50ee3b30518f9c75d20a9b9d798
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-04-10 06:16:41 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { AboutComponent } from './about/about.component';
-import { AcademicsComponent } from './academics/academics.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { AboutComponent } from "./about/about.component";
+import { AcademicsComponent } from "./academics/academics.component";
+import { CertComponent } from "./cert/cert.component";
+import { ThoughtsComponent } from "./thoughts/thoughts.component";
+import { AttemptsComponent } from "./attempts/attempts.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'academics', component: AcademicsComponent },
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "detail/:id", component: HeroDetailComponent },
+  { path: "heroes", component: HeroesComponent },
+  { path: "about", component: AboutComponent },
+  { path: "academics", component: AcademicsComponent },
+  { path: "about", component: AboutComponent },
+  { path: "cert", component: CertComponent },
+  { path: "thoughts", component: ThoughtsComponent },
+  { path: "attempts", component: AttemptsComponent },
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
