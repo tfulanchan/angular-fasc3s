1b125ca037391e28e0c4b9f1ea129dc7157a7f43
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-04-10 06:26:09 -0400

    Add files via upload

---
+++ src/app/thoughts/thoughts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thoughts',
+  templateUrl: './thoughts.component.html',
+  styleUrls: ['./thoughts.component.css']
+})
+export class ThoughtsComponent {
+
+}
